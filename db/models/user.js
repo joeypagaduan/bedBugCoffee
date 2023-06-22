44b0c99ad7f65ecc49679b86113e52802951c66f
trantuyen1411 <trantuyen1411@gmail.com>
2023-06-22 14:28:21 -0700

    working on user db

--- db/models/user.js
+++ db/models/user.js
@@ -1,11 +1,104 @@
 // grab our db client connection to use with our adapters
-const client = require('../client');
+const client = require("./client");
+const bcrypt = require("bcrypt");
 
-module.exports = {
-  // add your database adapter fns here
-  getAllUsers,
-};
+async function createUser({ username, password, email }) {
+  const SALT_COUNT = 5;
+  const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
+
+  try {
+    const { rows: [user] } = await client.query(
+      `
+      INSERT INTO users(username, password, email)
+      VALUES($1, $2, $3)
+      ON CONFLICT (username) DO NOTHING
+      RETURNING *;
+    `,
+      [username, hashedPassword, email]
+    );
+
+    delete user.password;
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getUser({ username, password }) {
+  const user = await getUserByUsername(username);
+  const hashedPassword = user.password;
+
+  const isValid = await bcrypt.compare(password, hashedPassword);
+
+  if (isValid) {
+    delete user.password;
+    return user;
+  }
+}
+
+async function getUserById(userId) {
+  try {
+    const { rows: [user] } = await client.query(
+      `
+      SELECT *
+      FROM users
+      WHERE id=$1
+    `,
+      [userId]
+    );
+
+    delete user.password;
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getUserByUsername(username) {
+  try {
+    const { rows: [user] } = await client.query(
+      `
+      SELECT *
+      FROM users
+      WHERE username=$1;
+    `,
+      [username]
+    );
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
 
 async function getAllUsers() {
   /* this adapter should fetch a list of users from your db */
+  try {
+    const query = 'SELECT * FROM users';
+    const { rows } = await client.query(query);
+
+    // Remove password field from each user object
+    const users = rows.map((user) => {
+      const { password, ...userWithoutPassword } = user;
+      return userWithoutPassword;
+    });
+
+    return users;
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    throw error;
+  }
 }
+
+module.exports = {
+  createUser,
+  getUser,
+  getUserById,
+  getUserByUsername,
+  getAllUsers,
+};
