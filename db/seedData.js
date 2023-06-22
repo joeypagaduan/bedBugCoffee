82e4b624b1b6d0bc208e192bedff59a4e4bd8cea
Joey Pagaduan <joey.paga220@gmail.com>
2023-06-22 10:20:44 -0700

    added UNIQUE modifier to email in userdb

--- db/seedData.js
+++ db/seedData.js
@@ -33,7 +33,7 @@ async function createTables() {
       CREATE TABLE users(
         id SERIAL PRIMARY KEY,
         username varchar(255) UNIQUE NOT NULL,
-        email varchar(255) NOT NULL,
+        email varchar(255) UNIQUE NOT NULL,
         password varchar(255) NOT NULL
       );
 
