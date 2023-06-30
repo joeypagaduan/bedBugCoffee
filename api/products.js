ccf1e4beb14cb1bdd44025f473c903a699c13c16
pinones97 <pinones.810@gmail.com>
2023-06-29 20:11:41 -0700

    added //POST /api/products to api/products.js

--- api/products.js
+++ api/products.js
@@ -1,107 +1,75 @@
-const express = require('express');
-const { createProduct, getAllProducts } = require('../db/models/products');
+const express = require("express");
 const router = express.Router();
 
-// GET /api/products
-router.get('/', async (req, res, next) => {
-  console.log('Products router');
-  try {
-    const products = await getAllProducts();
-    console.log('API ', products);
+const {
+    addProducts,
+    getProductByName,
+    getProductById,
+    getAllProducts
+} = require("../db/");
 
-    res.send(products);
-  } catch ({ name, message }) {
-    res.send({ name, message });
-  }
-});
-
-// POST /api/products
+//POST /api/products
 router.post('/', async (req, res, next) => {
-  const { ingredients, name, price, calories } = req.body;
-  const products = await createProduct({
-    ingredients,
-    name,
-    price,
-    calories,
-  });
+    try{
+        const {
+            productName,
+            ingredients,
+            price,
+            calories,
+            inventory
+        } = req.body;
+        
+        const existingProduct = await getProductByName(productName);
 
-  res.send(products);
+        if (existingProduct) {
+            return next({
+                error: "ProductError",
+                message: "Product " + productName + " is already added.",
+                name: productName
+            });
+        }
+
+        const newProduct = await addProducts({productName, ingredients, price, calories, inventory});
+
+        res.send({
+            message: "You have added a new Product.",
+            user: {
+                id: productName.id,
+                productName: newProduct.productName,
+                ingredients: newProduct.ingredients,
+                price: newProduct.price,
+                calories: newProduct.calories,
+                inventory: newProduct.inventory
+            },
+        });
+    }
+    
+    catch (error) {
+        next(error);
+    }
+
 });
 
-// PATCH /api/products/:productsId
+//GET /api/products
+router.get('/', async (req, res, next) => {
 
-// router.patch('/:routineId', async (req, res, next) => {
-//   const { isPublic, name, goal } = req.body;
-//   const { routineId } = req.params;
+});
 
-//   if (!req.headers.authorization) {
-//     res.send({
-//       error: 'Error',
-//       message: 'You must be logged in to perform this action',
-//       name: 'Logged in error',
-//     });
-//   } else {
-//     const rTest = await getRoutineById(routineId);
-//     const user = jwt.decode(req.headers.authorization.replace('Bearer ', ''), {
-//       complete: true,
-//     });
-//     if (rTest.creatorId != user.payload.id) {
-//       res.status(403).send({
-//         error: 'Error',
-//         message: `User ${user.payload.username} is not allowed to update Every day`,
-//         name: 'Logged in error',
-//       });
-//     }
+//GET /api/products/:productId
+router.get('/:productId', async (req, res, next) => {
 
-//     const routine = await updateRoutine({
-//       id: routineId,
-//       isPublic,
-//       name,
-//       goal,
-//     });
+});
 
-//     res.send(routine);
-//   }
-// });
+//PATCH /api/products/:productId
+router.patch('/:productId', async (req, res, next) => {
 
-// // DELETE /api/products/:routineId
+});
 
-// router.delete('/:routineId', async (req, res, next) => {
-//   const { routineId } = req.params;
+//DELETE /api/products/:productId
+router.delete('/:productId', async (req, re+s, next) => {
 
-//   const rTest = await getRoutineById(routineId);
-//   const user = jwt.decode(req.headers.authorization.replace('Bearer ', ''), {
-//     complete: true,
-//   });
-//   if (rTest[0].creatorId != user.payload.id) {
-//     res.status(403).send({
-//       error: 'Error',
-//       message: `User ${user.payload.username} is not allowed to delete On even days`,
-//       name: 'Logged in error',
-//     });
-//   } else {
-//     const routine = await getRoutineById(routineId);
-//     const deleted = await destroyRoutine(routineId);
+});
 
-//     res.send(routine);
-//   }
-// });
 
-// // POST /api/routines/:routineId/activities
-// router.post('/:routineId/activities', async (req, res, next) => {
-//   const { routineId } = req.params;
-//   const { activityId } = req.body;
-
-//   try {
-//     const routines = await addActivityToRoutine(req.body);
-//     res.send(routines);
-//   } catch (error) {
-//     res.send({
-//       error: '',
-//       message: `Activity ID ${activityId} already exists in Routine ID ${routineId}`,
-//       name: '',
-//     });
-//   }
-// });
 
 module.exports = router;
