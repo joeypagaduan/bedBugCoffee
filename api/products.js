7e962c8269a6b3137cf00f8b80484408ef2273fb
pinones97 <pinones.810@gmail.com>
2023-07-06 20:24:06 -0700

    added //GET /api/products and //GET /api/products/:productId

--- api/products.js
+++ api/products.js
@@ -19,9 +19,9 @@ router.post('/', async (req, res, next) => {
             inventory
         } = req.body;
         
-        const existingProduct = await getProductByName(productName);
+        const existingProduct = await getProductByName({productName});
 
-        if (existingProduct) {
+        if (!existingProduct) {
             return next({
                 error: "ProductError",
                 message: "Product " + productName + " is already added.",
@@ -33,14 +33,14 @@ router.post('/', async (req, res, next) => {
 
         res.send({
             message: "You have added a new Product.",
-            user: {
-                id: productName.id,
+            addedProduct: {
+                id: newProduct.id,
                 productName: newProduct.productName,
                 ingredients: newProduct.ingredients,
                 price: newProduct.price,
                 calories: newProduct.calories,
                 inventory: newProduct.inventory
-            },
+            }
         });
     }
     
@@ -52,11 +52,51 @@ router.post('/', async (req, res, next) => {
 
 //GET /api/products
 router.get('/', async (req, res, next) => {
+    
+    try{
+        const {
+            
+        } = req.body;
+
+        const products = await addProducts();
+        res.send({
+            message: "these are all the products",
+            products: products
+        });
+    }
+
+    catch (error) {
+        next(error);
+    }
 
 });
 
 //GET /api/products/:productId
 router.get('/:productId', async (req, res, next) => {
+    try {
+        const {
+            productId
+        } = req.body;
+
+        const product = await getProductById({ productId });
+
+        if (!product) {
+            return next({
+                error: "ProductError",
+                message: "Product  with the id of " + productId  + " is not avalible.",
+            });
+        }
+
+        res.send({
+            message: "this is the proudct with the id of " + productId,
+            products: product
+        });
+    }
+
+    catch (error) {
+        next(error);
+    }
+
 
 });
 
@@ -66,7 +106,7 @@ router.patch('/:productId', async (req, res, next) => {
 });
 
 //DELETE /api/products/:productId
-router.delete('/:productId', async (req, re+s, next) => {
+router.delete('/:productId', async (req, res, next) => {
 
 });
 
