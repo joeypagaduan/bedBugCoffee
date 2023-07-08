253d8134d5bd081697b1a2429301f596b170cb29
Damylles Rose <damylleskopper@gmail.com>
2023-07-07 19:22:59 -0700

    fixed client db

--- db/client.js
+++ db/client.js
@@ -4,38 +4,26 @@
 // // change the DB_NAME string to whatever your group decides on
 // const DB_NAME = 'bedbugcofe';
 // change the DB_NAME string to whatever your group decides on
-const { Pool } = require("pg");
+const { Client } = require('pg');
 const DB_NAME = 'bedBugCoffee';
 
-
-// const DB_URL =
-//   process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
-
-// let client;
-
-// // github actions client config
-// if (process.env.CI) {
-//   client = new Client({
-//     host: 'localhost',
-//     port: 5432,
-//     user: 'postgres',
-//     password: 'postgres',
-//     database: 'postgres',
-//   });
-// } else {
-//   // local / heroku client config
-//   client = new Client(DB_URL);
-// }
-
-const connectionString =
+const DB_URL =
   process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
 
-const client = new Pool({
-  connectionString,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : undefined,
-});
+let client;
+
+// github actions client config
+if (process.env.CI) {
+  client = new Client({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'postgres',
+  });
+} else {
+  // local / heroku client config
+  client = new Client(DB_URL);
+}
 
 module.exports = client;
