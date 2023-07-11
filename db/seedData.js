c32c445cbc57031c7b23f4e3358764f8001ded79
Joey Pagaduan <joey.paga220@gmail.com>
2023-07-10 21:45:35 -0700

    still debugging

--- db/seedData.js
+++ db/seedData.js
@@ -1,21 +1,31 @@
-const client = require('./client');
+// require in the database adapter functions as you write them (createUser, createProduct...)
+// const { } = require('./');
+const client = require("./client")
 
-const { createUser, addProducts } = require('./models');
+const {
+  createUser,
+  addProducts,
+  createAdmin,
+} = require("./");
 
 async function dropTables() {
   try {
-    console.log('Dropping All Tables...');
-    // drop all tables, in the correct order
+    console.log("Dropping All Tables...")
+  // drop all tables, in the correct order
 
     await client.query(`
+      DROP TABLE IF EXISTS order_items;
+      DROP TABLE IF EXISTS orders;
+      DROP TABLE IF EXISTS cart_item;
+      DROP TABLE IF EXISTS carts;
       DROP TABLE IF EXISTS products;
-      DROP TABLE IF EXISTS users;
       DROP TABLE IF EXISTS admin;
-    `);
+      DROP TABLE IF EXISTS users;
+    `)
 
     console.log('Finished dropping tables!');
   } catch (error) {
-    console.error('Error while dropping tables!', error);
+    console.error('Error while dropping tables!');
 
     throw error;
   }
@@ -23,15 +33,14 @@ async function dropTables() {
 
 async function createTables() {
   try {
-    console.log('Starting to build tables...');
-
+    console.log("Starting to build tables...")
     // create all tables, in the correct order
     await client.query(`
       CREATE TABLE users(
         id SERIAL PRIMARY KEY,
         username varchar(255) UNIQUE NOT NULL,
-        email varchar(255) UNIQUE NOT NULL,
-        password varchar(255) NOT NULL
+        password varchar(255) NOT NULL,
+        email varchar(255) UNIQUE NOT NULL
       );
 
       CREATE TABLE admin(
@@ -39,8 +48,8 @@ async function createTables() {
         username varchar(255) UNIQUE NOT NULL,
         email varchar(255) UNIQUE NOT NULL,
         password varchar(255) NOT NULL
-      );
-
+        );
+        
       CREATE TABLE products(
         id SERIAL PRIMARY KEY,
         "productName" VARCHAR(255) UNIQUE NOT NULL,
@@ -48,15 +57,45 @@ async function createTables() {
         price DECIMAL(4, 2) NOT NULL,
         calories INTEGER NOT NULL,
         inventory INTEGER
+        );
+          
+      CREATE TABLE carts(
+        "cartId" SERIAL PRIMARY KEY,
+        "userId" INTEGER
+        );
+            
+      CREATE TABLE cart_item(
+        "cartProductId" SERIAL PRIMARY KEY,
+        "cartProductName" VARCHAR(255) UNIQUE NOT NULL,
+        "cartProductDescription" VARCHAR(255) NOT NULL,
+        "cartQuantity" INTEGER NOT NULL,
+        "cartProductPrice" DECIMAL(10, 2) NOT NULL,
+        "cartTotalPrice" DECIMAL(10, 2) NOT NULL,
+        "cartId" INTEGER NOT NULL,
+        FOREIGN KEY ("cartId") REFERENCES carts("cartId")
+        );
+        
+        CREATE TABLE orders(
+        "orderId" SERIAL PRIMARY KEY,
+        "orderDate" TIMESTAMP NOT NULL,
+        "userId" INTEGER NOT NULL,
+        "orderTotalPrice" DECIMAL(10, 2) NOT NULL DEFAULT 0.00
       );
+      
+      CREATE TABLE order_items(
+        id SERIAL PRIMARY KEY,
+        "orderProductName" VARCHAR(255) UNIQUE NOT NULL,
+        "orderProductPrice" DECIMAL(4, 2) NOT NULL,
+        "orderQty" INTEGER NOT NULL
+        );
     `);
-    console.log('Finished building tables!');
-  } catch (error) {
-    console.error('Error building tables!');
-
-    throw error;
+        console.log('Finished building tables!');
+      } catch (error) {
+        console.error('Error building tables!');
+        
+        throw error;
   }
-}
+};
 
 /* 
 
@@ -65,155 +104,137 @@ SEED DATA
 */
 
 async function createInitialUsers() {
+  console.log("Starting to create users...")
   try {
     const usersToCreate = [
-      {
-        username: 'damylles',
-        email: 'damylles@hello.edu',
-        password: 'rose123',
-      },
-      { username: 'tuyen', email: 'tuyen@hello.edu', password: 'tran123' },
-      {
-        username: 'mirian',
-        email: 'mirian@hello.edu',
-        password: 'piniones123',
-      },
-    ];
-    const users = await Promise.all(usersToCreate.map(createUser));
+      { username: "damylles", password: "rose123", email: "damylles@hello.edu" },
+      { username: "tuyen", password: "tran123", email: "tuyen@hello.edu" },
+      { username: "mirian", password: "piniones123", email: "mirian@hello.edu" },
+    ]
+    const users = await Promise.all(usersToCreate.map(createUser))
 
-    console.log('Users created:');
-    console.log(users);
-    console.log('Finished creating users!');
+    console.log("Users created:")
+    console.log(users)
+    console.log("Finished creating users!")
   } catch (error) {
-    console.error('Error creating users!');
-    throw error;
+    console.error("Error creating users!")
+    throw error
   }
-}
+};
 
 async function createInitialAdmin() {
-  console.log('Starting to create administrators...');
+  console.log("Starting to create administrators...")
   try {
     const adminToCreate = [
-      {
-        username: 'controller',
-        email: 'admin@bedbugcafe.com',
-        password: 'iControlAll',
-      },
-    ];
-    const admin = await Promise.all(adminToCreate.map(createAdmin));
+      { username: "controller", email: "admin@bedbugcafe.com", password: "iControlAll" },
+    ]
+    const admin = await Promise.all(adminToCreate.map(createAdmin))
 
-    console.log('Admin created:');
-    console.log(admin);
-    console.log('Finished creating administrators!');
+    console.log("Admin created:")
+    console.log(admin)
+    console.log("Finished creating administrators!")
   } catch (error) {
-    console.error('Error creating administrators!');
-    throw error;
+    console.error("Error creating administrators!")
+    throw error
   }
 }
 
 async function createInitialProducts() {
   try {
-    console.log('Starting to create products...');
+    console.log("Starting to create products...")
 
     const productsToCreate = [
       {
-        productName: 'House Black',
-        ingredients: 'Fresh ground arabica beans',
-        price: '4.50',
-        calories: '5',
-        inventory: '10',
+        productName: "House Black",
+        ingredients: "Fresh ground arabica beans",
+        price: "4.50",
+        calories: "5",
+        inventory: "10"
       },
       {
-        productName: 'Latte',
-        ingredients:
-          'A shot of espresso and your choice of the type of steamed milk with a touch of foam',
-        price: '6.50',
-        calories: '190',
-        inventory: '10',
+        productName: "Latte",
+        ingredients: "A shot of espresso and your choice of the type of steamed milk with a touch of foam",
+        price: "6.50",
+        calories: "190",
+        inventory: "10"
       },
       {
-        productName: 'Cappuccino',
-        ingredients:
-          'A shot of espresso, generous foam with a sprinkle of cocoa powder and cinnamon',
-        price: '6.50',
-        calories: '160',
-        inventory: '10',
+        productName: "Cappuccino",
+        ingredients: "A shot of espresso, generous foam with a sprinkle of cocoa powder and cinnamon",
+        price: "6.50",
+        calories: "160",
+        inventory: "10"
       },
       {
-        productName: 'Americano',
-        ingredients: 'Concentrated espresso shot, diluted in water',
-        price: '5',
-        calories: '18',
-        inventory: '10',
+        productName: "Americano",
+        ingredients: "Concentrated espresso shot, diluted in water",
+        price: "5",
+        calories: "18",
+        inventory: "10"
       },
       {
-        productName: 'Doppio',
-        ingredients: 'A double shot of our special espresso',
-        price: '5.50',
-        calories: '10',
-        inventory: '10',
+        productName: "Doppio",
+        ingredients: "A double shot of our special espresso",
+        price: "5.50",
+        calories: "10",
+        inventory: "10"
       },
       {
-        productName: 'Cortado',
-        ingredients: 'The perfect balance of espresso and warm steamed milk',
-        price: '5.50',
-        calories: '15',
-        inventory: '10',
+        productName: "Cortado",
+        ingredients: "The perfect balance of espresso and warm steamed milk",
+        price: "5.50",
+        calories: "15",
+        inventory: "10"
       },
       {
-        productName: 'Red Eye',
-        ingredients: 'Our House Black with a shot of espresso mixed in',
-        price: '6.50',
-        calories: '8',
-        inventory: '10',
+        productName: "Red Eye",
+        ingredients: "Our House Black with a shot of espresso mixed in",
+        price: "6.50",
+        calories: "8",
+        inventory: "10"
       },
       {
-        productName: 'Macchiato',
-        ingredients:
-          'Espresso, milk, and foam. The happy medium between a cappuccino and doppio',
-        price: '7',
-        calories: '206',
-        inventory: '10',
+        productName: "Macchiato",
+        ingredients: "Espresso, milk, and foam. The happy medium between a cappuccino and doppio",
+        price: "7",
+        calories: "206",
+        inventory: "10"
       },
       {
-        productName: 'Irish *must be 21 or older',
-        ingredients:
-          'Our House Black, whiskey, and sugar, topped with freshly whipped cream',
-        price: '15',
-        calories: '154',
-        inventory: '10',
+        productName: "Irish *must be 21 or older",
+        ingredients: "Our House Black, whiskey, and sugar, topped with freshly whipped cream",
+        price: "15",
+        calories: "154",
+        inventory: "10"
       },
-    ];
-    const products = await Promise.all(productsToCreate.map(addProducts));
+    ]
+    const products = await Promise.all(productsToCreate.map(addProducts))
 
-    console.log('products created:');
-    console.log(products);
+    console.log("products created:")
+    console.log(products)
 
-    console.log('Finished creating products!');
+    console.log("Finished creating products!")
   } catch (error) {
-    console.error('Error creating products!');
-    throw error;
+    console.error("Error creating products!")
+    throw error
   }
 }
 
 async function rebuildDB() {
   try {
-    await dropTables();
-    await createTables();
-    await createInitialUsers();
-    await createInitialProducts();
-    //await createInitialAdmin();
+    await dropTables()
+    await createTables()
+    await createInitialUsers()
+    await createInitialAdmin()
+    await createInitialProducts()
   } catch (error) {
-    console.log('Error during rebuildDB');
-    throw error;
+    console.log("Error during rebuildDB")
+    throw error
   }
 }
 
-module.exports = {
-  rebuildDB,
-  dropTables,
-  createTables,
-  createInitialUsers,
-  createInitialProducts,
-  createInitialAdmin,
-};
+client.connect();
+
+rebuildDB()
+  .catch(console.error)
+  .finally(() => client.end());
