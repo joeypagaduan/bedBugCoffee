1b5fce95629141dc60ebc830efb2944b5cd2d955
pinones97 <pinones.810@gmail.com>
2023-07-12 22:16:41 -0700

    added cart component

---
+++ src/components/Cart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Cart ({token}) {
+
+    console.log("cart page, load token: " + token)
+
+        return (
+            <>
+                <div>cart !!!!!!!!!!!!!!!!!!</div>
+            </>
+        );
+
+}
+
+export default Cart;
